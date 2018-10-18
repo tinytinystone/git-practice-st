c92a28469020a107de4c4d93c8c0eae9d30943c2
Hong Jeesoo <jeesoo.hong@gmail.com>
2018-10-19 08:53:29 +0900

    name 변수 생성

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const buttonEl = document.querySelector('button')
 buttonEl.addEventListener('click', e => {
   // alert('알림창이 떴습니다')
-  prompt('이름이 무엇인가요?')
+  const name = prompt('이름이 무엇인가요?')
 })
