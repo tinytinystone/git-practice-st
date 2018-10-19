4154004f75c998064dbdda85d8dae974570fa52b
Hong Jeesoo <jeesoo.hong@gmail.com>
2018-10-19 09:02:10 +0900

    textContent 추가

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const buttonEl = document.querySelector('button')
 buttonEl.addEventListener('click', e => {
   // alert('알림창이 떴습니다')
   const name = prompt('이름이 무엇인가요?')
+  document.querySelector('h1').textContent = 'name'
 })
